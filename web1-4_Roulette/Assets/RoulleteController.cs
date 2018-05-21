0eec859133a4be10e9176c03fe92e50ce34c7910
Unknown <g1827402@st.t-kougei.ac.jp>
2018-05-21 17:18:40 +0900

    回転スクリプト実装

---
+++ web1-4_Roulette/Assets/RoulleteController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoulleteController : MonoBehaviour {
+
+    float rotSpeed = 5;
+
+
+
+    int gennsoku = 0;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+        //if(this.gennsoku == 0)
+        //{
+        //    this.rotSpeed *= 1.1f;
+
+        //    if (this.rotSpeed >= 15)
+        //    {
+        //        gennsoku = 1;
+        //    }
+        //}
+
+        //if(gennsoku == 1)
+        //{
+        this.rotSpeed *= 0.98f;
+
+        //    if(this.rotSpeed < 0)
+        //    {
+        //        gennsoku = 2;
+        //        this.rotSpeed = 5;
+        //    }
+        //}
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        //this.rotSpeed *= 0.96f;
+    }
+}
